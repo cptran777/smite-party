44bd9b073b35c5516bd853214e284ae7e1658326
cptran777 <cptran14@gmail.com>
2016-09-20 15:38:26 -0700

    node server now serves up basic file when called upon.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 /******************* INIT DEPENDENCIES *********************/
 
 var express = require('express');
+var bodyparser = require('body-parser');
 
 var app = express();
 
@@ -11,6 +12,14 @@ router(app);
 /******************* INIT MIDDLEWARE ***********************/
 
 app.use(express.static(__dirname + '/../client/public'));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(bodyparser.json());
 
 /********************* INIT SERVER *************************/
 
--- server/routes.js
+++ server/routes.js
@@ -1,3 +1,11 @@
+var path = require('path');
+
 module.exports = function(app) {
   
+  var sendFile = function(req, res) {
+    res.sendFile(path.resolve(__dirname + '/../client/public/index.html'));
+  };
+  
+  app.get('/', sendFile);
+
 };
