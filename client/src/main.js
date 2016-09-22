729fa9b481243b336fb62d5dcb2c7c728d5ee9b1
cptran777 <cptran14@gmail.com>
2016-09-21 21:20:30 -0700

    modify gitignore and setup main file that will be webpacked

---
+++ client/src/main.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+/******************* COMPONENT IMPORTS *********************/
+
+import App from './containers/App';
+
+/********************* REDUX IMPORTS ***********************/
+
+import { Provider } from 'react-redux';
+import configureStore from './store/configStore';
+
+const store = configureStore();
+
+/********************* REDUX IMPORTS ***********************/
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path='/' component={App}>
+        <IndexRoute component={}/>
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('app')
+);
