d5b3e134b70c8cc325c5b04e60cd8904efef9f5c
cptran777 <cptran14@gmail.com>
2016-09-22 17:19:52 -0700

    new webpack stuff to see if it works.

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,48 @@ const PATHS = {
   src: path.join(__dirname, 'client/src/main.js')
 };
 
+// module.exports = {
+//   entry: [
+//     PATHS.src
+//   ],
+//   output: {
+//     path: PATHS.compiled,
+//     filename: 'app.bundle.js'
+//   },
+//   module: {
+//     loaders: [
+//       {
+//         test: /\.jsx?$/,
+//         //exclude: /scripts/,
+//         include: __dirname + '/client/src',
+//         loader: 'babel-loader',
+//         query: {
+//           presets: ['react', 'es2015']
+//         }
+//       }
+//     ]
+//   },
+//   resolveLoaders: {
+//     modulesDirectories: ['node_modules']
+//   },
+//   resolve: { // in import statements default to these file types if none specified
+//     extensions: ['', '.js', '.jsx'],
+//     modulesDirectories: ['node_modules']
+//   },
+//   devServer: {
+//     contentBase: PATHS.compiled,
+//     historyApiFallback: true,
+//     hot: true,
+//     inline: true,
+//     progress: true
+//   },
+//   plugins: [
+//     // new webpack.HotModuleReplacementPlugin()
+//   ]
+// };
+
+var webpack = require('webpack');
+
 module.exports = {
   entry: [
     PATHS.src
@@ -14,34 +56,22 @@ module.exports = {
     path: PATHS.compiled,
     filename: 'app.bundle.js'
   },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx?$/,
-        //exclude: /scripts/,
-        include: __dirname + '/client/src',
-        loader: 'babel-loader',
-        query: {
-          presets: ['react', 'es2015']
-        }
-      }
-    ]
-  },
-  resolveLoaders: {
-    modulesDirectories: ['node_modules']
-  },
-  resolve: { // in import statements default to these file types if none specified
-    extensions: ['', '.js', '.jsx'],
-    modulesDirectories: ['node_modules']
-  },
-  devServer: {
-    contentBase: PATHS.compiled,
-    historyApiFallback: true,
-    hot: true,
-    inline: true,
-    progress: true
-  },
   plugins: [
-    // new webpack.HotModuleReplacementPlugin()
-  ]
+    new webpack.ProvidePlugin({
+      'React': 'react'
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel-loader',
+      include: __dirname + '/client/src',
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
 };
